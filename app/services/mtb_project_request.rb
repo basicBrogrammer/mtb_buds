8f29dd3efb382bdc205742ec39fe1984947f60cd
Jeremy Ward <jeremy.ward@mckesson.com>
2018-04-07 14:05:22 -0600

    make mtb project service object

---
+++ app/services/mtb_project_request.rb
@@ -0,0 +1,46 @@
+class MtbProjectRequest
+  ROOT_URL="https://www.mtbproject.com/data"
+  VALID_END_POINTS = ['get-trails', 'get-trails-by-id']
+  # https://www.mtbproject.com/data/get-trails?
+  #   ?lat=40.0274&lon=-105.2519&maxDistance=10&maxResult
+  # https://www.mtbproject.com/data/get-trails-by-id
+  #   ?ids=4670265,3671983,7015764,7003838,157369
+  attr_reader :endpoint, :params
+  def initialize(endpoint:, params: {})
+    if VALID_END_POINTS.include? endpoint
+      @params = params
+      @endpoint = endpoint
+    else
+      raise "Invalid endpoint: #{endpoint}. Accepted endpoints: #{VALID_END_POINTS}"
+    end
+  end
+
+  def call
+    response = get_json
+    JSON.parse(response.body)['trails']
+  end
+
+  def get_json
+    Rails.cache.fetch(cache_key, expires_in: 7.days) do
+      client.get do |req|                           # GET http://sushi.com/search?page=2&limit=100
+        req.headers['Content-Type'] = 'application/json'
+        req.url endpoint
+        req.params = get_params
+      end
+    end
+  end
+
+  def get_params
+    params.merge(key: ENV['mtb_project_key'])
+  end
+
+  def client
+    @connection ||= Faraday.new(url: ROOT_URL)
+  end
+
+  private
+
+  def cache_key
+    "[GET]mtb/#{endpoint}: #{params.to_query}"
+  end
+end
--- todo.md
+++ todo.md
@@ -1,7 +1,15 @@
 * [ ] rides
 
+  * testing
+    * [ ] no edit/update
+    * [ ] user specific values
   * [ ] pick trail
   * [ ] pagination/infinite load
+
+* [ ] mtb api
+
+  * [ ] test validation
+  * [ ] error request
 
 * [ ] gravatar
 * [ ] MV materialize to webpack
