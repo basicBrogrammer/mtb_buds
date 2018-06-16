0a91b9c7c510606141a4d46cdb25a61787f7d8eb
Jeremy Ward <jeremy.ward@mckesson.com>
2018-06-15 19:49:58 -0600

    add DOKKU_SCALE file

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,2 +1,3 @@
 # Resque.logger.level = Logger::DEBUG
 #Resque.logger = Logger.new(Rails.root.join('log', "#{Rails.env}_resque.log"))
+Resque.redis = Rails.env.production? ? ENV['REDIS_URL'] : 'localhost:6379'
