6e7762fd71c3596058b93185590ea615ed3c5018
Jeremy Ward <jeremy.ward@mckesson.com>
2018-06-19 22:47:45 -0600

    rm bad relationship

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :ride
   belongs_to :user
-  has_many :notifications, as: :action, dependent: :destroy
 
   after_create_commit { CommentBroadcastJob.perform_now(self, ride) }
   after_create { CreateCommentNotificationJob.perform_later(self) }
