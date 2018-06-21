39c1ac7acfe84d50db961d4ff584ecf4629c8582
Jeremy Ward <jeremy.ward@mckesson.com>
2018-06-21 06:03:31 -0600

    hide mobile nav on comment input focus

--- app/javascript/controllers/comment_controller.js
+++ app/javascript/controllers/comment_controller.js
@@ -1,10 +1,14 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "scroll" ]
+  static targets = [ "scroll", 'input' ]
 
   connect() {
     this.scrollTarget.scrollTop = this.scrollTarget.scrollHeight
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+      $(this.inputTarget).focus(() => this._handleInputFocus())
+      $(this.inputTarget).blur(() => this._handleInputBlur())
+    }
   }
 
   submit(event) {
@@ -15,5 +19,20 @@ export default class extends Controller {
 
     const commentCard = $('.card--comments')[0]
     commentCard.scrollIntoView()
+  }
+
+  _handleInputFocus(e) {
+    this._mobileNav.hide()
+    this._cardAction.css('bottom', 0)
+  }
+  _handleInputBlur(e) {
+    this._mobileNav.show()
+    this._cardAction.css('bottom', '5%')
+  }
+  get _mobileNav() {
+    return $('.mobile-bottom-nav');
+  }
+  get _cardAction() {
+    return $('.card--comments .card-action')
   }
 }
