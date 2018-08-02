39a9901f38f954cd94a7453af82b05f93dbabe20
Jeremy Ward <jeremy.ward@mckesson.com>
2018-08-02 16:15:25 -0600

    update unread_notifications slack message

--- app/jobs/mailers/unread_notifications_job.rb
+++ app/jobs/mailers/unread_notifications_job.rb
@@ -3,16 +3,20 @@
 module Mailers
   class UnreadNotificationsJob
     def self.perform
-      self.slack_notification
-
+      new_emails = 0
       User.find_each(batch_size: 500) do |user|
-        NotificationsMailer.unread(user).deliver_now if user.notifications.unread.any?
+        if user.notifications.unread.any?
+          NotificationsMailer.unread(user).deliver_now
+          new_emails +=1
+        end
       end
+
+      self.slack_notification(new_emails)
     end
 
-    def self.slack_notification
+    def self.slack_notification(new_emails)
       notifier = Slack::Notifier.new ENV['slack_hook']
-      notifier.ping("#{self} (#{Rails.env}) performing")
+      notifier.ping("#{self} (#{Rails.env}) sent ##{new_emails} new emails.")
     end
   end
 end
