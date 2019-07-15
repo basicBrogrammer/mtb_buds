b5fe429c821cf31305d377e865ef3fadbcb2ece6
Jeremy Ward <jeremy.ward@mckesson.com>
2019-07-14 20:15:37 -0400

    update webpacker

--- app/javascript/controllers/choose_trail_controller.js
+++ app/javascript/controllers/choose_trail_controller.js
@@ -1,27 +1,21 @@
-import Select2LocationController from './select2_location_controller'
+import Select2LocationController from './select2_location_controller';
 
 export default class extends Select2LocationController {
-  static targets = [
-    "select",
-    "url",
-    "trailId",
-    "preloader",
-    "trails"
-  ];
+  static targets = ['select', 'url', 'trailId', 'preloader', 'trails'];
 
   selectTrail(e) {
     this.trailIdTarget.value = e.target.dataset.trailId;
   }
 
   _onSelect(location) {
-    $(this.trailsTarget).addClass("hide");
-    $(this.preloaderTarget).removeClass("hide");
-    return fetch(this.trailsUrl(location), { credentials: "same-origin" })
-      .then(response => response.text())
-      .then(html => {
+    $(this.trailsTarget).addClass('hide');
+    $(this.preloaderTarget).removeClass('hide');
+    return fetch(this.trailsUrl(location), { credentials: 'same-origin' })
+      .then((response) => response.text())
+      .then((html) => {
         this.trailsTarget.innerHTML = html;
-        $(this.trailsTarget).removeClass("hide");
-        $(this.preloaderTarget).addClass("hide");
+        $(this.trailsTarget).removeClass('hide');
+        $(this.preloaderTarget).addClass('hide');
       });
   }
 
